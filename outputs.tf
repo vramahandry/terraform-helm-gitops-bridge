84ff4df3043d3197b78f44a032ef00414e498887
vramahandry <56304555+vramahandry@users.noreply.github.com>
2025-02-24 09:22:22 +0100

    fix: add sensitive to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
 output "argocd" {
   description = "Argocd helm release"
   value       = try(helm_release.argocd[0], null)
+  sensitive = true
 }
 output "cluster" {
   description = "ArgoCD cluster"
   value       = try(kubernetes_secret_v1.cluster[0], null)
+  #sensitive = true
 }
 output "apps" {
   description = "ArgoCD apps"
   value       = try(helm_release.bootstrap, null)
+  sensitive = true
 }
